b87753b3a9e26b844eafd514a134fa10c15fece2
Barbarossa <clement@beemoov.com>
2021-03-21 13:10:37 +0100

    Dessin en commune

--- There Was A Plan/Assets/Scripts/Drawer.cs
+++ There Was A Plan/Assets/Scripts/Drawer.cs
@@ -1,16 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Drawer : MonoBehaviour
+public class Drawer : MonoBehaviourPunCallbacks, IPunObservable
 {
+    Vector3 mousePos;
+    bool isDrawing;
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(mousePos);
+            stream.SendNext(isDrawing);
+        }
+        else
+        {
+            mousePos = (Vector3)stream.ReceiveNext();
+            isDrawing = (bool)stream.ReceiveNext();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (photonView.IsMine)
+        { 
+            if (Input.GetMouseButton(0))
+            {
+                isDrawing = true;
+                mousePos = Input.mousePosition;
+            }
+            else
+            {
+                isDrawing = false;
+            }
+        }
+        if(isDrawing)
         {
             Plane objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
-            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray mRay = Camera.main.ScreenPointToRay(mousePos);
             float rayDistance;
             GetComponent<TrailRenderer>().enabled = true;
             if (objPlane.Raycast(mRay, out rayDistance))
